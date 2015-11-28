f0b4672f49a6c83ac97e90506b99bf4b7b5baf17
TonyHsu <sunprince12014@gmail.com>
2015-11-29 00:24:03 +0800

    llllllots modified

--- DE2_115/src/Main.sv
+++ DE2_115/src/Main.sv
@@ -13,6 +13,13 @@ module Main(
 	input i_aud_bclk,
 
 	input [7:0] i_sw
+	inout io_sram_dq,
+	output o_sram_oe,
+	output o_sram_we,
+	output o_sram_ce,
+	output o_sram_lb,
+	output o_sram_ub,
+	output o_sram_addr
 	//output [31:0] debug
 );
 
@@ -28,20 +35,33 @@ localparam PTR_READ=2;
 localparam LEFT=0;
 localparam RIGHT=1;
 
+localparam FALSE=0;
+localparam TRUE=1;
+
 localparam CHANNEL_LENGTH=16;
 
 logic[2:0] main_state_r, main_state_w;
 logic init_finish;
 logic [1:0] mem_ptr_type_r, mem_ptr_type_w;
 logic start_i2s_r,start_i2s_w;
+logic data_write_in_w,data_write_in_r;
+logic rst_ptr;
+logic data_read_out_w,data_read_out_r;
+logic beIncAddr_r,beIncAddr_w;//increment 1 unit(depend on SRamMgr setting) if this addr is full
+logic [3:0] left_cnt_r,left_cnt_w;
+logic [3:0] right_cnt_r,right_cnt_w;
+logic [3:0] sram_dq_loc;
 
-
+assign rst_ptr = (main_state_r == S_IDLE)?TRUE:FALSE;
 
 always_comb begin
 	main_state_w = main_state_r;
 	debug_w = debug_r;
 	mem_ptr_type_w = mem_ptr_type_r;
 	start_i2s_w = start_i2s_r;
+	data_write_in_w = data_write_in_r;
+	data_read_out_w = data_read_out_r;
+	sram_dq_loc = 0;
 
 	case(main_state_r)
 		S_INIT: begin
@@ -60,6 +80,37 @@ always_comb begin
 		S_RECORD:begin
 			if(i_sw[1] == 1 && i_sw[2] == 0) begin
 				main_state_w = S_RECORD;
+				if(start_i2s_r==0)begin //new peroid
+					mem_ptr_type_w = PTR_DUMMY;
+					start_i2s_w = 1;//wait one clock
+					beIncAddr_w = FALSE;
+				end
+				else begin
+					mem_ptr_type_w = PTR_WRITE;
+					data_write_in_w = i_aud_adcdat;
+					if(i_aud_adclrck == LEFT)begin
+						if(left_cnt_r == CHANNEL_LENGTH-1)begin
+							beIncAddr_w = TRUE;
+							left_cnt_w = 0;
+							start_i2s_w = FALSE;
+						end
+						else begin
+							left_cnt_w = left_cnt_r + 1;
+						end
+						sram_dq_loc=left_cnt_r;
+					end
+					else begin
+						if(right_cnt_r == CHANNEL_LENGTH-1)begin
+							beIncAddr_w = TRUE;
+							right_cnt_w = 0;
+							start_i2s_w = FALSE;
+						end
+						else begin
+							right_cnt_w = right_cnt_r + 1;
+						end
+						sram_dq_loc=right_cnt_r;
+					end
+				end
 			end
 			else begin
 				main_state_w = S_IDLE;
@@ -69,6 +120,37 @@ always_comb begin
 		S_PLAY:begin
 			if(i_sw[1] == 0 && i_sw[2] == 1) begin
 				main_state_w = S_PLAY;
+				if(start_i2s_r==0)begin //new peroid
+					mem_ptr_type_w=PTR_DUMMY;
+					start_i2s_w = 1;//wait one clock
+					beIncAddr_w = FALSE;
+				end
+				else begin
+					mem_ptr_type_w=PTR_READ;
+					o_aud_dacdat = data_read_out_r;
+					if(i_aud_daclrck == LEFT)begin
+						if(left_cnt_r == CHANNEL_LENGTH-1)begin
+							beIncAddr_w = TRUE;
+							left_cnt_w = 0;
+							start_i2s_w = FALSE;
+						end
+						else begin
+							left_cnt_w = left_cnt_r + 1;
+						end
+						sram_dq_loc=left_cnt_r;
+					end
+					else begin
+						if(right_cnt_r == CHANNEL_LENGTH-1)begin
+							beIncAddr_w = TRUE;
+							right_cnt_w = 0;
+							start_i2s_w = FALSE;
+						end
+						else begin
+							right_cnt_w = right_cnt_r + 1;
+						end
+						sram_dq_loc=right_cnt_r;
+					end
+				end
 			end
 			else begin
 				main_state_w = S_IDLE;
@@ -102,13 +184,16 @@ end
 		.i_clk(i_clk_100k),
 		.i_rst_n(i_rst_n),
 		.i_ptr_type(mem_ptr_type_r),
-		.i_data_writein(i_aud_adcdat),
+		.i_rst_ptr(rst_ptr),
+		.i_data_writein(data_write_in_r),
 		.i_bePtrMove(i_sw[0]),
 		.i_speed(i_sw[7:5]),
 		.i_beSlow(i_sw[3]),
-		.o_data_readout(o_aud_dacdat),
+		.i_beIncAddr(beIncAddr_r)
+		.o_data_readout(data_read_out_r),
 		//.i_beReverse(i_sw[4]),
 		.io_sram_dq(io_sram_dq),
+		.i_sram_dq_loc(sram_dq_loc),
 		.o_sram_oe(o_sram_oe),
 		.o_sram_we(o_sram_we),
 		.o_sram_ce(o_sram_ce),
--- DE2_115/src/SRamMgr.sv
+++ DE2_115/src/SRamMgr.sv
@@ -3,13 +3,16 @@ module SRamMgr(
 	input i_rst_n,
 	//input i_rst_ptr,
 	input[1:0] i_ptr_type,
+	input i_rst_ptr,
 	input i_data_writein,
 	input i_bePtrMove,
 	input[2:0] i_speed,
 	input i_beSlow,
+	input i_beIncAddr,
 	output o_data_readout,
 	//input beRerverse,
 	inout[15:0] io_sram_dq,
+	input[3:0] i_sram_dq_loc,
 
 	output o_sram_oe,
 	output o_sram_we,
@@ -22,7 +25,7 @@ module SRamMgr(
 );
 
 logic[15:0] data;
-logic[2:0] addr_inc_r,addr_inc_w;
+logic[2:0] addr_inc;
 logic[19:0] addr_r,addr_w;
 
 localparam PTR_DUMMY=0;
@@ -36,6 +39,7 @@ localparam ADDR_LAST = 20'b1;
 assign o_sram_addr = addr_r;
 assign io_sram_dq = data;
 assign ptr_type = i_bePtrMove?i_ptr_type:PTR_DUMMY
+assign addr_inc = i_beIncAddr?(i_speed):0
 assign oe = (ptr_type==PTR_READ)?1:0
 
 
@@ -56,7 +60,7 @@ Task Record;
 		o_sram_oe = 1'bz;
 		o_sram_lb = 0;
 		o_sram_ub = 0;
-		io_sram_dq = i_data_writein;
+		io_sram_dq[i_sram_dq_loc] = i_data_writein;
 	end
 endtask
 
@@ -73,33 +77,36 @@ endtask
 
 inout_port io_sram(
 	.i_oe(oe),
-	.io(io_sram_dq),
+	.io(io_sram_dq[i_sram_dq_loc]),
 	.i_o(o_data_readout)
 );
 
 
 always_comb begin
-	
-	//if(i_rst_ptr)begin
-		//addr_w = ADDR_FIRST;
-	//end
+	if(i_rst_ptr)begin
+		DoNoting();
+		addr_w = ADDR_FIRST;
+	end
+	addr_w = addr_r;
 	case(ptr_type)
 		PTR_DUMMY:begin
 			DoNoting();
 		end
 		PTR_WRITE:begin
-			if((addr_r + addr_inc_r) > ADDR_LAST)begin
+			if((addr_r + addr_inc) > ADDR_LAST)begin
 				DoNoting();
 			end
 			else begin
+				addr_w = addr_r + addr_inc;
 				Record();
 			end
 		end
 		PTR_READ:begin
-			if((addr_r + addr_inc_r > ADDR_LAST)) begin
-				addr_r = 
+			if((addr_r + addr_inc > ADDR_LAST)) begin
+				addr_w = ADDR_FIRST;
 			end
 			else begin
+				addr_w = addr_r + addr_inc;
 				Play();
 			end
 		end
